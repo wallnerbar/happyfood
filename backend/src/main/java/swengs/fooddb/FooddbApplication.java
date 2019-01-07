252b8c503a9f7ab267682cb858b71183f2a0499c
rinnhoferthomas <thomas.rinnhofer@edu.fh-joanneum.at>
2019-01-07 12:21:01 +0100

    first commit

---
+++ backend/src/main/java/swengs/fooddb/FooddbApplication.java
@@ -0,0 +1,14 @@
+package swengs.fooddb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FooddbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FooddbApplication.class, args);
+    }
+
+}
+
