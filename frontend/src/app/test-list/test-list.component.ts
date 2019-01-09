b281394c8ecb13f0e5e61a131c7313d46103fe57
VeeLeifert <verena.leifert@edu.fh-joanneum.at>
2019-01-09 16:06:45 +0100

    test-list.component
    scss,ts und html geändert… startseite mit leiste oben, bild und code für 3 neueste rezepte anzeigen

--- frontend/src/app/test-list/test-list.component.ts
+++ frontend/src/app/test-list/test-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-test-list',
   templateUrl: './test-list.component.html',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestListComponent implements OnInit {
 
+  neuesteRezeptListe;
+
   constructor() { }
 
   ngOnInit() {
+    //getRezepteListe() muss implementiert werden... gibt alle Rezepte aus
+    // speichert alle rezepte in eine liste und holt die 3 mit die höchsten indexes in die neuesteRezeptListe
+
+    liste;
+
+    liste = getRezepteListe();
+    neuesteRezeptListe.push(liste.eintrag[eintrag.length - 1], liste.eintrag[eintrag.length - 2], liste.eintrag[eintrag.length - 3]);
+  }
+
   }
 
 }
